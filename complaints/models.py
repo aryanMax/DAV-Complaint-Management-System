bb25d0b57e5768c8585ee9e9319cd313d8ccedc9
Aryan Rana <105802182+aryanMax@users.noreply.github.com>
2026-04-06 21:47:39 +0530

    Update models.py

--- complaints/models.py
+++ complaints/models.py
@@ -1,3 +1,6 @@
+from django.db import models
+from django.contrib.auth.models import User
+
 class Complaint(models.Model):
     CATEGORY_CHOICES = (
         ('ACADEMIC', 'Academic'),
@@ -17,7 +20,7 @@ class Complaint(models.Model):
     description = models.TextField()
     category = models.CharField(max_length=20, choices=CATEGORY_CHOICES)
     status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='PENDING')
-    image = models.ImageField(upload_to='complaint_images/', blank=True, null=True) # NEW FIELD
+    image = models.ImageField(upload_to='complaint_images/', blank=True, null=True)
     created_by = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
 
